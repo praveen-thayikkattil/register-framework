997b28b2d1961680bdfd77d94fb396ebbe6456e9
Praveen Ramachandran <praveen.thayikkattil@gmail.com>
2020-08-22 15:42:40 +0530

    UpdatEs #1 for initial skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <p>PIT Solutitions</p>
+        <buttton
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer"1
         >
-          Learn React
-        </a>
+          Register
+        </buttton>
       </header>
     </div>
   );
