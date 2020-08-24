b2ca682e932907bf87535d25c0729e4c894a4172
Praveen Ramachandran <praveen.thayikkattil@gmail.com>
2020-08-24 23:12:45 +0530

    material-ui implementation plus profile page

--- src/FormFillingPage.jsx
+++ src/FormFillingPage.jsx
@@ -2,6 +2,91 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import Input from "react-select";
 import Select from "react-select";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Slider from "@material-ui/core/Slider";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+const marks = [
+  {
+    value: 0,
+    label: "13 - 19",
+  },
+  {
+    value: 25,
+    label: "20 - 29",
+  },
+  {
+    value: 60,
+    label: "30 - 45",
+  },
+  {
+    value: 100,
+    label: "45+",
+  },
+];
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+function CheckboxLabels() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedF: true,
+    checkedG: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <formGroup row>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={state.checkedA}
+            onChange={handleChange}
+            name="checkedA"
+          />
+        }
+        label="Subscribe to the newsletter"
+      />
+    </formGroup>
+  );
+}
+
+function DiscreteSlider() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Slider
+        defaultValue={25}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider-custom"
+        step={10}
+        valueLabelDisplay="auto"
+        marks={marks}
+      />
+    </div>
+  );
+}
 
 class FormFillingPage extends Component {
   render() {
@@ -11,116 +96,164 @@ class FormFillingPage extends Component {
       { value: "texas", label: "Texas" },
     ];
 
+    const countriesOptions = [
+      { value: "new-york", label: "New York" },
+      { value: "california", label: "California" },
+      { value: "canada", label: "Canada" },
+    ];
+
+    const addressesOptions = [
+      { value: "address-one", label: "Address One" },
+      { value: "address-two", label: "Address Two" },
+      { value: "address-three", label: "Address Three" },
+    ];
+
+    const interestsOptions = [
+      { value: "hockey", label: "Hockey" },
+      { value: "football", label: "Football" },
+      { value: "reading", label: "Reading" },
+    ];
+
     return (
       <header className="App-body">
         <h4>Please fill out the form below</h4>
 
         <div className="registration-form">
-          <div className="fieldset-name">
+          <div className="fieldset-name fieldset">
             <div className="control-label">
               <label>Name</label>
             </div>
 
-            <Input />
+            <TextField
+              id="outlined-basic"
+              label="First Name"
+              variant="outlined"
+              className="first-name-input"
+            />
 
-            <input type="text" placeholder="First Name" />
-
-            <input type="text" placeholder="Last Name" />
+            <TextField
+              id="outlined-basic"
+              label="Last Name"
+              variant="outlined"
+              className="last-name-input"
+            />
           </div>
 
-          <div className="fieldset-agerange">
+          <div className="fieldset-agerange fieldset">
             <div className="control-label">
               <label>Age</label>
             </div>
 
-            <input type="range" min="13" max="45" width="100%" />
+            <Slider
+              defaultValue={20}
+              getAriaValueText={valuetext}
+              aria-labelledby="discrete-slider-custom"
+              step={5}
+              valueLabelDisplay="auto"
+              marks={marks}
+            />
           </div>
 
-          <div className="fieldset-email">
+          <div className="fieldset-email fieldset">
             <div className="control-label">
               <label>Email</label>
             </div>
 
-            <input type="email" placeholder="john@doe.com" />
+            <TextField
+              id="outlined-basic"
+              label="john@doe.com"
+              variant="outlined"
+            />
           </div>
 
-          <div className="fieldset-tel">
+          <div className="fieldset-tel fieldset">
             <div className="control-label">
               <label>Tel</label>
             </div>
 
-            <input type="tel" placeholder="(212) 477 - 1000" />
+            <TextField
+              id="outlined-basic"
+              label="(212) 477 - 1000"
+              variant="outlined"
+            />
           </div>
 
-          <div className="fieldset-state">
+          <div className="fieldset-state fieldset">
             <div className="control-label">
               <label>State</label>
             </div>
 
-            <Select options={statesOptions} />
-
-            <select>
-              <option>Alaska</option>
-            </select>
+            <TextField id="selectState" label="" value="20" select>
+              <MenuItem value="10">New York</MenuItem>
+              <MenuItem value="20">California</MenuItem>
+              <MenuItem value="30">Florida</MenuItem>
+            </TextField>
           </div>
 
-          <div className="fieldset-country">
+          <div className="fieldset-country fieldset">
             <div className="control-label">
               <label>Country</label>
             </div>
 
-            <select>
-              <option>United States</option>
-            </select>
+            <TextField id="selectCountry" label="" value="30" select>
+              <MenuItem value="10">USA</MenuItem>
+              <MenuItem value="20">UK</MenuItem>
+              <MenuItem value="30">India</MenuItem>
+            </TextField>
           </div>
 
-          <div className="fieldset-address">
+          <div className="fieldset-address fieldset">
             <div className="control-label">
               <label>Address</label>
             </div>
 
-            <select>
-              <option>Please select</option>
-            </select>
+            <div className="textAreaAutosize-wrapper">
+              <TextareaAutosize
+                aria-label="empty textarea"
+                placeholder="Type your address here"
+                rowsMin={3}
+              />
+            </div>
           </div>
 
-          <div className="fieldset-interests">
+          <div className="fieldset-interests fieldset">
             <div className="control-label">
               <label>Interests</label>
             </div>
 
-            <input type="text" placeholder="Hockey, Football, Reading" />
-
-            <div className="interest-options">
-              <ul>
-                <li>
-                  <button>Hockey</button>
-                </li>
-                <li>
-                  <button>Football</button>
-                </li>
-                <li>
-                  <button>Reading</button>
-                </li>
-              </ul>
+            <div className="textfield-wrap">
+              <TextField
+                id="standard-full-width"
+                // label="Label"
+                // style={{ margin: 8 }}
+                placeholder="Hockey, Football, Reading"
+                // helperText="Full width!"
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="outlined"
+              />
             </div>
           </div>
 
-          <div className="fieldset-newsletter-choice">
+          <div className="fieldset-newsletter-choice fieldset">
             <div className="control-label">
-              <input type="checkbox" checked />
-              <label>
-                <span>Subscribe to the newsletter</span>
-              </label>
+              <CheckboxLabels />
             </div>
           </div>
 
-          <button
-            className="form-submit-button"
+          <br />
+
+          <Button
+            variant="contained"
+            color="primary"
             onClick={() => console.log("submitted the form...")}
+            className="form-submit-button"
           >
             Submit
-          </button>
+          </Button>
         </div>
 
         <h1>Form Filling Page</h1>
--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 class ProfilePage extends Component {
   render() {
     return (
-      <header className="App-body">
-        <h1>Profile Page</h1>
+      <header className="App-body profile-page">
+        <aside className="sidebar">
+          <div className="inner-wrap">
+            <div className="avatar-wrap">
+              <div className="the-img" />
+            </div>
+          </div>
+
+          <footer className="action-buttons">
+            <button className="change-photo-button">Change Photo</button>
+
+            <button className="edit-profile-button">Edit Profile</button>
+          </footer>
+        </aside>
+
+        <main class="main-content">
+          <p>
+            I am <span>Mr. David Miller</span> and I am{" "}
+            <span>above 20 years</span> and you can send your emails to{" "}
+            <span>jesus@heaven.com</span>.I live in the state of Alaska. I like
+            to <span>play hockey, football and reading</span> books. And please
+            send me the newsletters. Please reach out to me on my phone
+            9876543210.
+          </p>
+
+          <footer>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => console.log("submitted the form...")}
+              className="profile-agree-button"
+            >
+              Agree
+            </Button>
+          </footer>
+        </main>
+        {/*<h1>Profile Page</h1>
 
         <ul className="list-inline">
           <li>
@@ -19,7 +55,7 @@ class ProfilePage extends Component {
               Back to Home
             </NavLink>
           </li>
-        </ul>
+        </ul> */}
       </header>
     );
   }
